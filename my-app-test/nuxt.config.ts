2854f2791900517619794b85db3124b171c58c17
vorapong sangpiyaphet <vorapong.san@mahidol.ac.th>
2024-03-02 12:35:25 +0700

    add alias paths from tsconfig.base.json

--- my-app-test/nuxt.config.ts
+++ my-app-test/nuxt.config.ts
@@ -1,23 +1,50 @@
 import { nxViteTsPaths } from '@nx/vite/plugins/nx-tsconfig-paths.plugin';
 import { defineNuxtConfig } from 'nuxt/config';
+import { join } from 'path';
+import { workspaceRoot } from '@nx/devkit';
+import { fileURLToPath } from 'url';
+
+/**
+ * read the compilerOptions.paths option from a tsconfig and return as aliases for Nuxt
+ **/
+function getMonorepoTsConfigPaths(tsConfigPath: string) {
+  const tsPaths = require(tsConfigPath)?.compilerOptions?.paths as Record<
+    string,
+    string[]
+  >;
+
+  const alias: Record<string, string> = {};
+  if (tsPaths) {
+    for (const p in tsPaths) {
+      // '@org/something/*': ['libs/something/src/*'] => '@org/something': '{pathToWorkspaceRoot}/libs/something/src'
+      alias[p.replace(/\/\*$/, '')] = join(
+        workspaceRoot,
+        tsPaths[p][0].replace(/\/\*$/, '')
+      );
+    }
+  }
+
+  return alias;
+}
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   workspaceDir: '../',
   srcDir: 'src',
   devtools: { enabled: true },
+  alias: getMonorepoTsConfigPaths('../tsconfig.base.json'),
   devServer: {
     host: 'localhost',
     port: 4200,
   },
   typescript: {
     typeCheck: true,
-    tsConfig: {
-      extends: './tsconfig.app.json',
-    },
+    // tsConfig: {
+    //   extends: fileURLToPath(new URL('../tsconfig.base.json', import.meta.url)),
+    // },
   },
   imports: {
-    autoImport: true,
+    autoImport: false,
   },
 
   css: ['~/assets/css/styles.css'],
--- my-app-test/src/components/NxWelcome.vue
+++ my-app-test/src/components/NxWelcome.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import { useTest } from '#imports';
 import { test, testMe } from 'services';
 
 defineProps<{
